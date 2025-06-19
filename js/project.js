e629ef7039fe52579a72e55509f49bd0a4e9df19
hyeonji-noh <nnhyeonji@gmail.com>
2025-06-19 17:55:03 +0900

    postit

--- js/project.js
+++ js/project.js
@@ -1,16 +1,12 @@
 $(function () {
 
     var swiper = new Swiper(".tab_slide", {
-        slidesPerView: 6.5,
-        spaceBetween: 0,
+        slidesPerView: 7.6,
+        spaceBetween: 6,
         freeMode: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
-        },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
         },
     });
 })
