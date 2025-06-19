3737bba06ab89b2ec9d27d4e248a6f0e660e011c
yulssemnim <skf1011@naver.com>
2025-06-19 17:02:16 +0900

    project change

--- js/project.js
+++ js/project.js
@@ -1,1 +1,16 @@
-$(function(){})
+$(function () {
+
+    var swiper = new Swiper(".tab_slide", {
+        slidesPerView: 6.5,
+        spaceBetween: 0,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+})
